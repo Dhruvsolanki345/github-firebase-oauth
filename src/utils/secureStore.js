61f753f247a19c1b914893d861d9b6d0a9f99740
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2022-04-17 02:22:43 +0530

    Fix: expo secure store issue

--- src/utils/secureStore.js
+++ src/utils/secureStore.js
@@ -1,6 +1,6 @@
 import * as SecureStore from "expo-secure-store";
 
-const BASE_KEY = "github-firebase-oauth/";
+const BASE_KEY = "GITHUB-FIREBASE-OAUTH_";
 
 export const setItemToStore = async (key, value) => {
   try {
@@ -14,10 +14,18 @@ export const setItemToStore = async (key, value) => {
 export const getItemFromStore = async (key) => {
   try {
     const fullKey = BASE_KEY + key;
-    result = await SecureStore.getItemAsync(fullKey);
+    const result = await SecureStore.getItemAsync(fullKey);
     return result;
   } catch (err) {
     console.log("Error when getting value from expo secure store -> ", err);
   }
-  
+};
+
+export const removeItemFromStore = async (key) => {
+  try {
+    const fullKey = BASE_KEY + key;
+    result = await SecureStore.deleteItemAsync(fullKey);
+  } catch (err) {
+    console.log("Error when reomving value from expo secure store -> ", err);
+  }
 };
