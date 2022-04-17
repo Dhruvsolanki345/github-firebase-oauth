c53b8beabf8cd9a871545442a7b36eb457e31771
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2022-04-17 18:29:11 +0530

    Feature: login page completed

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,24 +1,60 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  useWindowDimensions,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { AntDesign } from "@expo/vector-icons";
 
 import GradientBtn from "../components/GradientBtn";
 
 import { useSetRecoilState } from "recoil";
 import { signInCallState } from "../store/user";
+import Images from "../../assets/Images";
 
 export default function Login() {
   const setIsSignInCall = useSetRecoilState(signInCallState);
+
+  const { width, height } = useWindowDimensions();
 
   const handleSignIn = () => {
     setIsSignInCall(true);
   };
 
   return (
-    <View>
-      <Text>Login</Text>
-      <GradientBtn onPress={handleSignIn} title="Login with github" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image
+        source={Images.login1}
+        resizeMode="contain"
+        style={{ width: width * 0.9, height: height * 0.65, borderRadius: 15 }}
+      />
+      <Text style={styles.header}>
+        Welcome to Firebase OAuth Application With GitHub
+      </Text>
+
+      <GradientBtn
+        onPress={handleSignIn}
+        containerStyle={{ width: width * 0.8 }}
+        title="Login with GitHub"
+        Icon={<AntDesign name="github" size={30} style={{marginLeft: 15}} color="#fff" />}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#05267A",
+    textAlign: "center",
+    lineHeight: 32,
+    marginBottom: 40,
+  },
+});
