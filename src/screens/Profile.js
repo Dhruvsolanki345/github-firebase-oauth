d80003e54930df6eb8fbdce1abdaeb3c9afd788e
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2022-04-17 18:25:47 +0530

    Fix: temporary signout btn added

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,10 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useRecoilValue } from 'recoil'
+import { userState } from '../store/user'
+import GradientBtn from '../components/GradientBtn';
+import { signOutAsync } from '../utils/auth';
 
 export default function Profile() {
+  const user = useRecoilValue(userState);
+
+  console.log({user})
+
   return (
     <View>
       <Text>Profile</Text>
+      <GradientBtn title="Sign Out" onPress={signOutAsync} />
     </View>
   )
 }
